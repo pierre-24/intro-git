6ca26a509a5690b8ff73d81d66461ba48af88df7
pierre-24 <pierreb24@gmail.com>
2024-12-14 13:48:03 +0100

    part2

---
+++ reset_for_part2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# A few command so that part 2 runs smoothly
+
+# add remote
+git remote add originx git@github.com:pierre-24/test-repo-git.git
+
+# fetch everything
+git fetch originx
+
+# reset main so that it matches what is locally there
+git push -u originx main -f
+
+# (re-)create a branch with a change wrt main, then delete it from local repository
+git branch -d a-remote-branch
+git push originx --delete a-remote-branch
+
+git switch -c a-remote-branch
+echo "test" > remote_file
+git add remote_file
+git commit -am "add remote_file"
+
+git switch main
+
+git push originx a-remote-branch
+git branch -d a-remote-branch
+
+# delete some other branches that are used in part2
+git push originx --delete an-example-branch
+git push originx --delete branch-fail-tests
+
+# delete remote
+git remote remove originx
